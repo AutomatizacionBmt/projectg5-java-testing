c90319919a597cf6222867ae37d335edd7ac30fb
Giancarlo <giancarlo2709@gmail.com>
2020-11-28 18:06:33 -0500

    verificacion credito instantaneo

--- src/main/java/clase2/metodos/VerificacionCreditoInstantaneo.java
+++ src/main/java/clase2/metodos/VerificacionCreditoInstantaneo.java
@@ -1,4 +1,52 @@
 package clase2.metodos;
 
+import java.util.Scanner;
+
+/**
+ * Escriba un programa de "verificación de crédito instantánea" que apruebe cualquier persona que gane más de S/ 25,000 y
+ * tenga un score de crédito de 700 o mejor. Rechaza a todos los demás.
+ */
 public class VerificacionCreditoInstantaneo {
+
+    static double salarioRequerido = 25000;
+    static int scoreCrediticioRequerido = 700;
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double salario = obtenerSalario();
+        int score = obtenerScoreCrediticio();
+
+        boolean estaCalificado = esUsuarioCalificado(salario, score);
+
+        System.out.println("Esta calificado? " + estaCalificado);
+
+        notificaUsuario(estaCalificado);
+
+    }
+
+    public static double obtenerSalario() {
+        System.out.println("Ingrese su salario: ");
+        return scanner.nextDouble();
+    }
+
+    public static int obtenerScoreCrediticio() {
+        System.out.println("Ingrese su score crediticio: ");
+        return scanner.nextInt();
+    }
+
+    public static boolean esUsuarioCalificado(double salario2, int score2) {
+        /* if(salario >= salarioRequerido && score >= scoreCrediticioRequerido) {
+            return true;
+        } else {
+            return false;
+        }*/
+        return salario2 >= salarioRequerido && score2 >= scoreCrediticioRequerido;
+    }
+
+    public static void notificaUsuario(boolean estaCalificado) {
+        System.out.println(estaCalificado
+                ? "Felicidades!, has aprobado el credito"
+                : "Lo Sentimos!, para otra oportunidad");
+    }
 }
