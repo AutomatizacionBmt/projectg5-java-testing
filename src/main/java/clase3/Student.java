e4f99f7953d23a88117623d94e8396755590b79b
Giancarlo <giancarlo2709@gmail.com>
2020-12-05 17:22:40 -0500

    add arrays multidimensionales

--- src/main/java/clase3/Student.java
+++ src/main/java/clase3/Student.java
@@ -1,5 +1,7 @@
 package clase3;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Student {
@@ -27,9 +29,24 @@ public class Student {
         int[][] numbers = new int[2][3];
 
         Random random = new Random();
+        Map<Integer, String> participants = new HashMap<>();
+        participants.put(1, "Ada");
+        participants.put(2, "Angelica");
+        participants.put(3, "Jair");
+        participants.put(4, "Johana");
+        participants.put(5, "Luis");
+        System.out.println("EL suertudo de la sesion es: " + participants.get(random.nextInt(6) + 1));
         for(int row = 0; row < 2; row++) {
             for(int column = 0; column < 3; column++) {
                 numbers[row][column] = random.nextInt(10) + 1;
+            }
+        }
+
+        for(int row = 0; row < 2; row++) {
+            System.out.println("Values row " + row);
+            for(int column = 0; column < 3; column++) {
+                System.out.println("Values column: " );
+                System.out.println(numbers[row][column]);
             }
         }
     }
