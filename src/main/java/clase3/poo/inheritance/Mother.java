1c42a9c054899b3851dab799785edf0110e16ad5
Giancarlo <giancarlo2709@gmail.com>
2020-12-19 15:09:13 -0500

    cadena de herencia

---
+++ src/main/java/clase3/poo/inheritance/Mother.java
@@ -0,0 +1,10 @@
+package clase3.poo.inheritance;
+
+import java.time.LocalDate;
+
+public class Mother extends Woman {
+
+    public Mother(String name, String lastName, String email, LocalDate birthDate) {
+        super(name, lastName, email, birthDate);
+    }
+}
---
+++ src/main/java/clase3/poo/inheritance/Woman.java
@@ -0,0 +1,10 @@
+package clase3.poo.inheritance;
+
+import java.time.LocalDate;
+
+public class Woman extends Person {
+
+    public Woman(String name, String lastName, String email, LocalDate birthDate) {
+        super(name, lastName, email, birthDate);
+    }
+}
